d5d79f3b4176b7d088aa83218176367d423706bf
monicagerber <mwgerber@gmail.com>
2021-02-23 15:38:57 -0800

    Function to use monolix preprocess template

---
+++ R/template-pk.R
@@ -0,0 +1,17 @@
+#' Use a Monolix PK Pre-Processing Template
+#'
+#' @param save_as where to save
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' use_monolix_preprocess(save_as = here::here("elisa", "01-preprocess.Rmd"))
+#' }
+use_monolix_preprocess() <- function(save_as = "01-preprocess.Rmd") {
+  usethis::use_template(
+    template = "monolix-step-01-preprocess.Rmd",
+    save_as = save_as,
+    package = "VISCtemplates"
+  )
+}
