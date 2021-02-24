c99a37f93ab2449dc73d94078b8dcad42fee062f
monicagerber <mwgerber@gmail.com>
2021-02-23 16:10:15 -0800

    Fix function name

--- R/template-pk.R
+++ R/template-pk.R
@@ -8,7 +8,7 @@
 #' \dontrun{
 #' use_monolix_preprocess(save_as = here::here("elisa", "01-preprocess.Rmd"))
 #' }
-use_monolix_preprocess() <- function(save_as = "01-preprocess.Rmd") {
+use_monolix_preprocess <- function(save_as = "01-preprocess.Rmd") {
   usethis::use_template(
     template = "monolix-step-01-preprocess.Rmd",
     save_as = save_as,
