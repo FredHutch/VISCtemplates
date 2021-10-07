2de1ba17577222bce1956a166a20b22aba636699
Monica Gerber <mwgerber@gmail.com>
2021-10-07 16:56:54 -0700

    Simplify and correct template

--- inst/templates/group-colors.R
+++ inst/templates/group-colors.R
@@ -1,19 +1,14 @@
 ## Group colors for {{ study_name }}
 
-# Edit the code below with group names and HEX colors.
-# The standard colors used at VISC are [edit].
-# The control group is usually designated with black (HEX # [edit]).
-# Non-response is usually designated with grey (HEX # [edit]).
-
 group_colors <- c(
-  `Group 1` = "",
-  `Group 2` = "",
-  `Group 3` = "",
-  `Group 4` = ""
+  `Group 1` = "[HEX#]",
+  `Group 2` = "[HEX#]",
+  `Group 3` = "[HEX#]",
+  `Group 4` = "[HEX#]"
 )
 
 # To source these group colors in your Rmarkdown report:
-#  knitr::read_chunk("protocol/group-colors.R")
+#  source("R/group-colors.R")
 
 # In ggplot2, you can set colors manually with:
 #  scale_color_manual(values = group_colors)
