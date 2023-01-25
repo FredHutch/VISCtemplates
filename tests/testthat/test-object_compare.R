e1b9f2d1d46a4edd3bb1ebf9e930b0a77ee53afe
valduran18 <106105256+valduran18@users.noreply.github.com>
2023-01-25 15:49:54 -0600

    Update test-object_compare.R

--- tests/testthat/test-object_compare.R
+++ tests/testthat/test-object_compare.R
@@ -1,20 +1,123 @@
 # tests for comparison of data objects
 context("test-git_object_compare")
 
-test_that("use full pdata name and suffix", {
-
-  # TODO: this can probably be abstracted to a minimum simple example
-  # using withr to create a temp directory see example:
-  # https://github.com/phuse-org/valtools/blob/main/tests/testthat/test-create_validation_packet.R
-
-  #Lusso847
-  repoList1 <- object_compare(dataObject = "nab", SHA1 = "f199ca8" , SHA2 = "30aa5d5")
-
-  repoList2 <- object_compare(dataObject = "Lusso847_nab", SHA1 = "f199ca8" , SHA2 = "30aa5d5")
-
+test_that("Use full pdata name. Expect Warning for Differences.", {
+   
+expect_warning(with_tempdir({
+  ## Create directories and initialize repositories
+  path_bare <- tempfile(pattern = "example")
+  path_repo_1 <- tempfile(pattern = "example")
+  
+  dir.create(path_bare)
+  dir.create(path_repo_1)
+  repo_bare <- init(path_bare, bare = TRUE)
+  ## Clone to repo 1 and config user
+  repo_1 <- clone(path_bare, path_repo_1)
+  config(repo_1, user.name = "User", user.email = "User@example.org")
+  
+  
+  ## Add changes to repo 1 and push to bare
+  df <- data.frame(x = c(1:10), y = c(11:20))
+  save(df, file = file.path(path_repo_1, "df.rda"))
+  
+  add(repo_1, "df.rda")
+  commit(repo_1, "data object v1")
+  push(repo_1, "origin", "refs/heads/master")
+  SHA1 <- last_commit(repo_1)$sha
+  
+  df <- data.frame(x = c(1:10), y = c(1:10))
+  save(df, file = file.path(path_repo_1, "df.rda"))
+  
+  add(repo_1, "df.rda")
+  commit(repo_1, "data object v2")
+  push(repo_1, "origin", "refs/heads/master")
+  SHA2 <- last_commit(repo_1)$sha
+  with_dir(path_repo_1, {
+    git_object_compare(data_object = "df",
+                       SHA1 = SHA1,
+                       SHA2 = SHA2)
+  })
+}),
+"Not all Values Compared Equal")
 })
 
+test_that("Use suffix pdata name. Expect Warning for Differences.", {
+   
+expect_warning(with_tempdir({
+  ## Create directories and initialize repositories
+  path_bare <- tempfile(pattern = "example")
+  path_repo_1 <- tempfile(pattern = "example")
+  
+  dir.create(path_bare)
+  dir.create(path_repo_1)
+  repo_bare <- init(path_bare, bare = TRUE)
+  ## Clone to repo 1 and config user
+  repo_1 <- clone(path_bare, path_repo_1)
+  config(repo_1, user.name = "User", user.email = "User@example.org")
+  
+  
+  ## Add changes to repo 1 and push to bare
+  df <- data.frame(x = c(1:10), y = c(11:20))
+  save(df, file = file.path(path_repo_1, "df.rda"))
+  
+  add(repo_1, "df.rda")
+  commit(repo_1, "data object v1")
+  push(repo_1, "origin", "refs/heads/master")
+  SHA1 <- last_commit(repo_1)$sha
+  
+  df <- data.frame(x = c(1:10), y = c(1:10))
+  save(df, file = file.path(path_repo_1, "df.rda"))
+  
+  add(repo_1, "df.rda")
+  commit(repo_1, "data object v2")
+  push(repo_1, "origin", "refs/heads/master")
+  SHA2 <- last_commit(repo_1)$sha
+  with_dir(path_repo_1, {
+    git_object_compare(data_object = "f",
+                       SHA1 = SHA1,
+                       SHA2 = SHA2)
+  })
+}),
+"Not all Values Compared Equal")
+})
 
+test_that("Use pdata object. Expect Text Indicating No Differences.", {
+  
+  expect_equal(with_tempdir({
+    ## Create directories and initialize repositories
+    path_bare <- tempfile(pattern = "example")
+    path_repo_1 <- tempfile(pattern = "example")
+    
+    dir.create(path_bare)
+    dir.create(path_repo_1)
+    repo_bare <- init(path_bare, bare = TRUE)
+    ## Clone to repo 1 and config user
+    repo_1 <- clone(path_bare, path_repo_1)
+    config(repo_1, user.name = "User", user.email = "User@example.org")
+    
+    
+    ## Add changes to repo 1 and push to bare
+    df <- data.frame(x = c(1:10), y = c(11:20))
+    save(df, file = file.path(path_repo_1, "df.rda"))
+    
+    add(repo_1, "df.rda")
+    commit(repo_1, "data object v1")
+    push(repo_1, "origin", "refs/heads/master")
+    SHA1 <- last_commit(repo_1)$sha
+    
+    df <- data.frame(x = c(1:10), y = c(1:10))
+    save(df, file = file.path(path_repo_1, "df.rda"))
+    
+    add(repo_1, "df.rda")
+    commit(repo_1, "data object v2")
+    push(repo_1, "origin", "refs/heads/master")
+    SHA2 <- last_commit(repo_1)$sha
+    with_dir(path_repo_1, {
+      git_object_compare(data_object = df)$differences
+    })
+  }),
+  "No issues were found!")
+})
 
 test_that("Warning Message is Shown When Differences Are Detected",{
   df<-data.frame(x=c(1:10),y=c(6:15))
