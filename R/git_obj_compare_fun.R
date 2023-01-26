6a3c2e6c7f0faf070968f4d9bd701343fa4b87ed
valduran18 <106105256+valduran18@users.noreply.github.com>
2023-01-25 18:58:42 -0600

    Update git_obj_compare_fun.R

--- R/git_obj_compare_fun.R
+++ R/git_obj_compare_fun.R
@@ -23,8 +23,41 @@
 #' @importFrom diffdf diffdf
 #' @details working directory must be pointed to repo clone
 #' @examples
-#' # Lusso847
-#' repoList <- git_object_compare(data_object = "nab", SHA1 = "f199ca8" , SHA2 = "30aa5d5")
+#' with_tempdir({
+#'  ## Create directories and initialize repositories
+#'  path_bare <- tempfile(pattern = "example")
+#'  path_repo_1 <- tempfile(pattern = "example")
+#' 
+#'  dir.create(path_bare)
+#'  dir.create(path_repo_1)
+#'  repo_bare <- init(path_bare, bare = TRUE)
+#'  ## Clone to repo 1 and config user
+#'  repo_1 <- clone(path_bare, path_repo_1)
+#'  config(repo_1, user.name = "User", user.email = "User@example.org")
+#'  
+#'  
+#'  ## Add changes to repo 1 and push to bare
+#'  df <- data.frame(x = c(1:10), y = c(11:20))
+#'  save(df, file = file.path(path_repo_1, "df.rda"))
+#'  
+#'  add(repo_1, "df.rda")
+#'  commit(repo_1, "data object v1")
+#'  push(repo_1, "origin", "refs/heads/master")
+#'  SHA1 <- last_commit(repo_1)$sha
+#'  
+#'  df <- data.frame(x = c(1:10), y = c(1:10))
+#'  save(df, file = file.path(path_repo_1, "df.rda"))
+#'  
+#'  add(repo_1, "df.rda")
+#'  commit(repo_1, "data object v2")
+#'  push(repo_1, "origin", "refs/heads/master")
+#'  SHA2 <- last_commit(repo_1)$sha
+#'  with_dir(path_repo_1, {
+#'    git_object_compare(data_object = "df",
+#'                       SHA1 = SHA1,
+#'                       SHA2 = SHA2)
+#'  })
+#'})
 #' @export
 git_object_compare <-
   function(data_object = NULL,
