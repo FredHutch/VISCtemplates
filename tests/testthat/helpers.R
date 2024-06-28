9d79bc676bfe69d913b7278b4969eebb08414579
Dave Slager <slager@users.noreply.github.com>
2024-06-28 10:52:52 -0700

    add inline comments

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,9 +1,17 @@
 #' Test knit from a report template
 #'
-#' @param report_type character, one of c('generic', 'empty', 'bama', 'nab')
-#' @param outfile_ext character, one of c('pdf', 'docx')
+#' Sets up an ephemeral temporary directory, creates a VISC project in it,
+#' creates a report of the specified type from the template, and knits it to the
+#' specified output type. Tests whether the report knits without errors or
+#' warnings, whether the output file exists, and takes file snapshots of the
+#' rendered output and/or intermediate debugging files to the
+#' tests/testthat/_snaps directory. See the documentation in test-template.R for
+#' a description of what happens when testing in different testing environments.
 #'
-#' @return side effects only
+#' @param report_type Character, one of c('generic', 'empty', 'bama', 'nab')
+#' @param outfile_ext Character, one of c('pdf', 'docx')
+#'
+#' @return Side effects only.
 test_knit_report <- function(report_type, outfile_ext){
   stopifnot(
     report_type %in% c('generic', 'empty', 'bama', 'nab'),
@@ -14,12 +22,15 @@ test_knit_report <- function(report_type, outfile_ext){
                      docx = 'VISCtemplates::visc_word_document')[outfile_ext]
   test_context <- paste('knit', report_type, 'report', outfile_ext)
   suppressWarnings({
+    # context() function is deprecated, but useful for test output readability
     testthat::context(test_context)
   })
   testthat::test_that(test_context, {
+    # creates ephemeral directory that will be deleted upon function exit
     temp_dir <- withr::local_tempdir()
     create_visc_project(temp_dir, interactive = FALSE)
     local({
+      # temporarily setwd() to temp_dir and undo upon exiting this local() block
       withr::local_dir(temp_dir)
       use_visc_report(report_name,
                       report_type = report_type,
@@ -31,11 +42,13 @@ test_knit_report <- function(report_type, outfile_ext){
                           quiet = TRUE,
                           clean = FALSE)
       )
+      # push this variable out of the local() block
       outfile_sans_ext <<- file.path(temp_dir, report_name, report_name)
     })
     expect_true(
       file.exists(paste0(outfile_sans_ext, '.', outfile_ext))
     )
+    # we'll try to take file snapshots of these file types
     try_snapshot_ext <- list(
       pdf = c('pdf', 'log', 'tex', 'md', 'Rmd', 'knit.md'),
       docx = c('docx', 'md', 'knit.md', 'Rmd')
@@ -45,6 +58,7 @@ test_knit_report <- function(report_type, outfile_ext){
         outfile_path <- paste0(outfile_sans_ext, '.', ext)
         if (file.exists(outfile_path)){
           suppressWarnings({
+            # don't want to see warning about initial snapshots
             expect_snapshot_file(outfile_path)
           })
         }
--- tests/testthat/test-template.R
+++ tests/testthat/test-template.R
@@ -1,3 +1,22 @@
+# This will test knitting all template report types, both pdf and docx. What
+# happens with file snapshots varies a bit depending on the testing context:
+#
+# When tests are run interactively via devtools::test(), all existing snapshots
+# in tests/testthat/_snaps/template in your VISCtemplates source repo are
+# deleted (because tests/testthat/setup.R is automatically run first) and
+# replaced with new snapshots. You shouldn't notice any changes in your git repo
+# because these are already .gitignored and .Rbuildignored.
+#
+# When tests are run inside R CMD check locally, e.g. via devtools::check(), the
+# testing happens from a temporary directory, so any existing files in
+# tests/testthat/_snaps/template within your VISCtemplates package source tree
+# are not changed. Check timestamps if you're confused.
+#
+# When the tests run on GitHub Actions CI, the snapshot files are uploaded as
+# artifacts for online viewing. Currently, if any of the tests fail, the file
+# snapshots will be buried within a larger results artifact, but when all tests
+# succeed, they will be in a snapshots artifact. Hopefully soon this will be
+# changed to always have the latter behavior regardless of test success.
 local({
   for (report_type in c('generic', 'empty', 'bama', 'nab')){
     for (output_ext in c('pdf', 'docx')){
