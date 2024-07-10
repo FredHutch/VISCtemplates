839e6f2dc1c16dc1ff64be7c69a3fd98a8e95545
Dave Slager <slager@users.noreply.github.com>
2024-07-09 23:33:11 -0700

    mv helper fn to visc_output_document.R

--- R/visc_output_document.R
+++ R/visc_output_document.R
@@ -97,3 +97,13 @@ visc_word_document <- function(toc = TRUE,
     number_sections = FALSE,
     ...)
 }
+
+find_resource <- function(template, file = 'template.tex', package = "VISCtemplates") {
+  res <- system.file(
+    "rmarkdown", "templates", template, "resources", file, package = package
+  )
+  if (res == "") stop(
+    "Couldn't find template file ", template, "/resources/", file, call. = FALSE
+  )
+  res
+}
