6d0664c5d2e95e491ebb6e4a2bc7a3f621ff9e5c
Kellie MacPhee <kmacphee@fredhutch.org>
2025-01-10 19:35:48 -0800

    Delete R/use_visc_tests.R

--- R/use_visc_tests.R
+++
@@ -1,9 +0,0 @@
-
-use_visc_spell_check_test <- function() {
-  usethis::use_template(
-    template = "test-spelling.R",
-    save_as = "tests/testthat/test-spelling.R",
-    data = list(),
-    package = "VISCtemplates"
-  )
-}
