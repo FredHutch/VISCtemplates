fec17a5f759a1a91c51acd1ed5bd37e9b946bda2
Kellie MacPhee <kmacphee@fredhutch.org>
2026-08-07 14:23:15 -0700

    attempt to fix template finding issue

--- R/use_slide_deck.R
+++ R/use_slide_deck.R
@@ -39,3 +39,31 @@ use_slide_deck <- function(deck_name = "VDCnnn_assay_slides",
 
   usethis::ui_done(glue::glue("Created slide deck at '{file.path(path, deck_name)}'"))
 }
+
+
+#' Custom PowerPoint output format for VISC slide decks
+#'
+#' Wraps [rmarkdown::powerpoint_presentation()], defaulting `reference_doc`
+#' to one of the Fred Hutch branded PPTX templates bundled with VISCtemplates.
+#'
+#' @param style Character, one of c("Gradient", "Light", "Navy"). Selects
+#'   which Fred Hutch branded PPTX template to use as `reference_doc`.
+#' @param ... Additional arguments passed to
+#'   [rmarkdown::powerpoint_presentation()], e.g. `slide_level`.
+#'
+#' @export
+visc_pptx_document <- function(style = c("Gradient", "Light", "Navy"), ...) {
+  style <- match.arg(style)
+  reference_doc <- system.file(
+    "templates", "slides",
+    paste0("Fred-Hutch-", style, "-Template.pptx"),
+    package = "VISCtemplates"
+  )
+  if (!nzchar(reference_doc)) {
+    stop(
+      "Could not find Fred Hutch '", style, "' PPTX template. ",
+      "Check that VISCtemplates is installed with its inst/templates/slides files intact."
+    )
+  }
+  rmarkdown::powerpoint_presentation(reference_doc = reference_doc, ...)
+}
