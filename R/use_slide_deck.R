c9464896ff9df1ba4534110060410aff13b85048
Kellie MacPhee <kmacphee@fredhutch.org>
2026-08-07 13:47:30 -0700

    attempt to fix errors

--- R/use_slide_deck.R
+++ R/use_slide_deck.R
@@ -1,7 +1,6 @@
-#' Use a Slide Deck Template
+#' Use a Slide Deck
 #'
-#' Creates a template R Markdown file for a slide presentation (PPTX/PDF),
-#' based on the layout in `slides_template.Rmd`.
+#' Creates a template R Markdown file for a slide presentation (PPTX)
 #'
 #' @param deck_name name of the file/folder (character, no extension, no subdirectory)
 #' @param path folder within the active project where the deck should be created
@@ -12,7 +11,7 @@
 #' @examples
 #' \dontrun{
 #' use_slide_deck(
-#'   deck_name = "McElrath708_TeamMeeting_Slides",
+#'   deck_name = "VDCnnn_Assay_Slides",
 #'   path = "slides"
 #' )
 #' }
@@ -26,18 +25,7 @@ use_slide_deck <- function(deck_name = "VDCnnn_assay_slides",
   old <- options(usethis.quiet = !interactive)
   on.exit(options(old))
 
-  if (!dir.exists(path)) {
-    dir.create(path, recursive = TRUE)
-    usethis::use_template(
-      template = "README_slides_folder.md",
-      data = list(deck_name = deck_name),
-      save_as = file.path(path, "README.md"),
-      package = "yourPackage"   # <- replace with your package name
-    )
-  }
-
-  # rmarkdown::draft() copies the whole skeleton/ folder for this template
-  # (including template.pptx for "branded"), so reference_doc just works.
+  # note: rmarkdown::draft() copies the whole skeleton/ folder for this template
   rmarkdown::draft(
     file = file.path(path, deck_name),
     template = system.file("templates", "slides", package = "VISCtemplates"),
